0bcd956ba97f5f6c1d0b77aae00d59797c4c5572
nguyenvanan123 <an070652@gmail.com>
2025-11-14 09:56:23 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import { PeerServer } from "peer";
+
+const app = express();
+
+app.use(cors());
+
+// PeerJS server
+const peerServer = PeerServer({
+  port: process.env.PORT || 9000,
+  path: "/peerjs"
+});
+
+app.use("/peerjs", (req, res) => {
+  res.send("PeerJS server is running!");
+});
+
+// Render uses one port → cần listen app
+app.listen(process.env.PORT || 9000, () => {
+  console.log("Server running...");
+});
